2134e46a7699c84b8d0ee8978cccc7ccb1e01b1d
Mor_aka_Gaia <mor.solomonov@gmail.com>
2020-04-04 02:47:22 +0200

    initial commit. added basic dependancies, Logo component and Login container Component for the Login page

---
+++ creatives-around-app/src/components/LoginContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from './Logo';
+
+export default function LoginContainer() {
+    return (
+        <div className="App-Container">
+            <Logo />
+        </div>
+    )
+}
