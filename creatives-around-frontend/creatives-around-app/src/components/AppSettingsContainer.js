d3b852167102414c2515e96fa5cb5e8db8f2bad0
Mor_aka_Gaia <mor.solomonov@gmail.com>
2020-04-11 03:51:34 +0200

    added images to AppSettingsContainer

--- creatives-around-frontend/creatives-around-app/src/components/AppSettingsContainer.js
+++ creatives-around-frontend/creatives-around-app/src/components/AppSettingsContainer.js
@@ -2,8 +2,10 @@ import React, { Fragment } from 'react'
 import '../styles/App.css';
 import TitleWithSymbol from './SymbolsandTitles/TitleWithSymbol';
 import Logo from './SymbolsandTitles/Logo';
-
-
+import AboutUsImage from '../images/graphs/about_us_box.svg';
+import DeleteAccountImage from '../images/graphs/delete_account_box.svg';
+import TermsImage from '../images/graphs/terms_conditions_box.svg';
+import ContactUsImage from '../images/graphs/contact_us_box.svg';
 
 const AppSettingsContainer = () => {
     return (
@@ -11,8 +13,18 @@ const AppSettingsContainer = () => {
             <div className="App-Container">
                 <TitleWithSymbol title="App Settings" />
                 <Logo />
-
+                <div className="About-Us-Buttons">
+                    <div>
+                        <img src={AboutUsImage} alt="AboutUsImage" />
+                        <img src={DeleteAccountImage} alt="DeleteAccountImage" />
+                    </div>
+                    <div>
+                        <img src={TermsImage} alt="TermsImage" />
+                        <img src={ContactUsImage} alt="ContactUsImage" />
+                    </div>
+                </div>
             </div>
+
         </Fragment>
     )
 
