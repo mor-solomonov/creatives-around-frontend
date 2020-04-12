c4793d57157f1de5e5a3f45aa650466c3311beb7
Mor_aka_Gaia <mor.solomonov@gmail.com>
2020-04-13 00:11:54 +0200

    added style and react router links to Activation Key Sent Page

--- creatives-around-frontend/creatives-around-app/src/components/ActivationKeySentContainer.js
+++ creatives-around-frontend/creatives-around-app/src/components/ActivationKeySentContainer.js
@@ -6,13 +6,16 @@ import GoBackButton from '../components/ButtonsAndCheckBoxs/GoBackButton'
 const ActivationKeySentContainer = () => {
     return (
 
-
         <div className="App-Container">
             <Logo />
-            <h3>An activation key has been sent to your email</h3>
-            <GoBackButton />
-        </div>
+            <h3 className="activationkeysent">An Activation key has been sent to your email.</h3>
+            <div className="text-center">
+            </div>
+            <div className="activation-key-gobackbutton">
 
+                <GoBackButton />
+            </div>
+        </div>
     )
 
 };
