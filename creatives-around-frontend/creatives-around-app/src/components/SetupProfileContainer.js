62ec44cca5b9ba4db15b41906a1c3dfc79c0fb49
Nic Solitom <nicsolitom@gmail.com>
2020-04-12 21:55:47 +0200

    Changed name from add_cover to add_cover_pic.svg, Added AddProfilePic & AddCoverPic to SetupProfileContainer.js, Added section for SetupProfilePics.js in App.css

--- creatives-around-frontend/creatives-around-app/src/components/SetupProfileContainer.js
+++ creatives-around-frontend/creatives-around-app/src/components/SetupProfileContainer.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react'
 import '../styles/App.css'
 import TitleWithSymbol from './SymbolsandTitles/TitleWithSymbol';
+import AddProfilePic from '../images/graphs/add_profile_pic.svg';
+import AddCoverPic from '../images/graphs/add_cover_pic.svg';
 
 
 
@@ -9,7 +11,20 @@ export default function SetupProfileContainer() {
         <Fragment>
             <div className="App-Container">
                 <TitleWithSymbol title="Setup Profile" />
+                <div className="flex-grid SetupProfilePics">
+                    <div className="col">
+                        <p></p>
+                    </div>
+                    <div className="col">
+                        <img src={AddProfilePic} alt="Add profile pic" id="AddProfilePic"/>
+                    </div>
+                    <div className="col">
+                        <img src={AddCoverPic} alt="Add cover pic" id="AddCoverPic"/>
+                    </div>
 
+
+                </div>
+                
             </div>
         </Fragment>
     )
