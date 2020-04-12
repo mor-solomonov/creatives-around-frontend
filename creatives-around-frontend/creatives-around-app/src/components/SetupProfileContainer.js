cc10964e6a3a0952a9a39b8651d6ae6e91df5c4b
Nic Solitom <nicsolitom@gmail.com>
2020-04-12 23:20:21 +0200

    Added 'Full Name'-field to SetupProfileContainer.js

--- creatives-around-frontend/creatives-around-app/src/components/SetupProfileContainer.js
+++ creatives-around-frontend/creatives-around-app/src/components/SetupProfileContainer.js
@@ -7,24 +7,50 @@ import AddCoverPic from '../images/graphs/add_cover_pic.svg';
 
 
 export default function SetupProfileContainer() {
+    const [state, setState] = React.useState({
+        displayFullName: false
+    });
+
+//! TO BE CHECKED:
+    const updateState = (e) => {
+        console.log('VALUE', e.target.value);
+        console.log('NAME', e.target.name);
+        setState({ ...state, [e.target.name]: e.target.value });
+    }
+//! ---->
     return (
         <Fragment>
-            <div className="App-Container">
-                <TitleWithSymbol title="Setup Profile" />
-                <div className="flex-grid SetupProfilePics">
-                    <div className="col">
+            <div className='App-Container'>
+                <TitleWithSymbol title='Setup Profile' />
+                
+                <div className='SetupProfilePics flex-grid'>
+                    <div className='col-4'>
                         <p></p>
                     </div>
-                    <div className="col">
-                        <img src={AddProfilePic} alt="Add profile pic" id="AddProfilePic"/>
+                    <div className='col-4'>
+                        <img src={AddProfilePic} alt='Add profile pic' id='AddProfilePic'/>
                     </div>
-                    <div className="col">
-                        <img src={AddCoverPic} alt="Add cover pic" id="AddCoverPic"/>
+                    <div className='col-4'>
+                        <img src={AddCoverPic} alt='Add cover pic' id='AddCoverPic'/>
                     </div>
-
-
                 </div>
                 
+                    <form>
+                <div className='FormFieldSetupProfile'>
+                    <h4 className='FormHeader'>Full Name</h4>
+                        <input
+                            className='FullName form-control mb-4'
+                            placeholder=''
+                            type='text'
+                            name='fullName'
+                            onChange={updateState}
+                        />
+                        <div className="DisplayFullNameWrapper">
+                            <input type='checkbox' id='checkbox' className='CheckboxFullNameCheckbox' name='displayFullName' value={state.displayFullName} onChange={updateState} />
+                            <label className='DisplayFullName'>&nbsp; &nbsp; Display Full name</label>
+                        </div>
+                </div>
+                    </form>
             </div>
         </Fragment>
     )
