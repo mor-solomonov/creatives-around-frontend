10c5df1cccfbc6a9b37ee1065dce9b6f1071bb80
Nic Solitom <nicsolitom@gmail.com>
2020-04-14 01:21:52 +0200

    Added: Cover and Profile pics

--- creatives-around-frontend/creatives-around-app/src/components/ViewMyProfileContainer.js
+++ creatives-around-frontend/creatives-around-app/src/components/ViewMyProfileContainer.js
@@ -1,13 +1,27 @@
 import React, { Fragment } from 'react'
 import '../styles/App.css';
 import TitleWithSymbol from './SymbolsandTitles/TitleWithSymbol';
-
+// Images:
+import ProfilePic from '../images/fake_profiles/profile_paintrbush_man.png';
+import CoverPic from '../images/fake_profiles/o-SPIEGELMAN-570.jpg';
 
 const ViewMyProfileContainer = () => {
     return (
         <Fragment>
-            <div className="App-Container">
-                <TitleWithSymbol title="My Profile" />
+            <div className='App-Container'>
+                <TitleWithSymbol title='My Profile' />
+                
+                <div className='ProfilePicsContainer'>
+
+                    <div className='CoverPic'>
+                        <img src={CoverPic} />
+                    </div>
+
+                    <div className='ProfilePic'>
+                        <img src={ProfilePic} />
+                    </div>
+                    
+                </div>
             </div>
         </Fragment>
     )
