24dd8d435d825e2238e6a8059900afb2f5d59fe6
Mor_aka_Gaia <mor.solomonov@gmail.com>
2020-04-13 02:34:51 +0200

    added react router links to AppSettings Page

--- creatives-around-frontend/creatives-around-app/src/components/AppSettingsContainer.js
+++ creatives-around-frontend/creatives-around-app/src/components/AppSettingsContainer.js
@@ -6,6 +6,8 @@ import AboutUsImage from '../images/graphs/about_us_box.svg';
 import DeleteAccountImage from '../images/graphs/delete_account_box.svg';
 import TermsImage from '../images/graphs/terms_conditions_box.svg';
 import ContactUsImage from '../images/graphs/contact_us_box.svg';
+import { Link } from 'react-router-dom';
+import BarsMenu from '../images/graphs/menu_bars.svg';
 
 const AppSettingsContainer = () => {
     return (
@@ -13,14 +15,17 @@ const AppSettingsContainer = () => {
             <div className="App-Container">
                 <TitleWithSymbol title="App Settings" />
                 <Logo />
+                {/* <div className="text-right">
+                    <img src={BarsMenu} className="BarsMenu" alt="BarsMenu" />
+                </div> */}
                 <div className="App-Settings-Buttons">
                     <div>
-                        <img src={AboutUsImage} className="App-Settings-Buttons" alt="AboutUsImage" />
-                        <img src={DeleteAccountImage} className="App-Settings-Buttons" alt="DeleteAccountImage" />
+                        <Link style={{ textDecoration: 'none' }} to='/aboutus'><img src={AboutUsImage} className="App-Settings-Buttons" alt="AboutUsImage" /> </Link>
+                        <Link style={{ textDecoration: 'none' }} to='/deleteaccount'><img src={DeleteAccountImage} className="App-Settings-Buttons" alt="DeleteAccountImage" /></Link>
                     </div>
                     <div>
-                        <img src={TermsImage} className="App-Settings-Buttons" alt="TermsImage" />
-                        <img src={ContactUsImage} className="App-Settings-Buttons" alt="ContactUsImage" />
+                        <Link style={{ textDecoration: 'none' }} to="/termsandconditions"><img src={TermsImage} className="App-Settings-Buttons" alt="TermsImage" /></Link>
+                        <Link style={{ textDecoration: 'none' }} to="/contactus"> <img src={ContactUsImage} className="App-Settings-Buttons" alt="ContactUsImage" /></Link>
                     </div>
                 </div>
             </div>
