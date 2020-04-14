74dcdff2810b867bcef81e5cd139eae3323a31d3
Mor_aka_Gaia <mor.solomonov@gmail.com>
2020-04-14 02:18:25 +0200

    Buttons and Links added to Connections Page

--- creatives-around-frontend/creatives-around-app/src/components/NotificationsContainer.js
+++ creatives-around-frontend/creatives-around-app/src/components/NotificationsContainer.js
@@ -1,13 +1,23 @@
 import React, { Fragment } from 'react'
 import '../styles/App.css';
 import TitleWithSymbol from './SymbolsandTitles/TitleWithSymbol';
-
+import Logo from './SymbolsandTitles/Logo';
+import MessagesImage from '../images/graphs/messages_box.svg'
+import ConnectionsImage from '../images/graphs/connections_box.svg'
+import { Link } from 'react-router-dom';
 
 const NotificationsContainer = () => {
     return (
         <Fragment>
             <div className="App-Container">
                 <TitleWithSymbol title="Notifications" />
+                <Logo />
+
+                <div className="App-Settings-Buttons">
+
+                    <Link style={{ textDecoration: 'none' }} to='/messages'><img src={MessagesImage} className="App-Settings-Buttons" alt="MessagesImage" /> </Link>
+                    <Link style={{ textDecoration: 'none' }} to='/connections'><img src={ConnectionsImage} className="App-Settings-Buttons" alt="ConnectionsImage" /></Link>
+                </div>
             </div>
         </Fragment>
     )
