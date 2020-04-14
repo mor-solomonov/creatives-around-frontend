227c5dd537e003fc6a2e3c9f8245a13747849af9
Mor_aka_Gaia <mor.solomonov@gmail.com>
2020-04-14 15:06:19 +0200

    added redirect function for Login Container - not complete

--- creatives-around-frontend/creatives-around-app/src/components/LoginContainer.js
+++ creatives-around-frontend/creatives-around-app/src/components/LoginContainer.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import Logo from './SymbolsandTitles/Logo';
 import DisplayTitle from './SymbolsandTitles/DisplayTitle';
 import { Link } from 'react-router-dom';
 
+
+
 export default function LoginContainer() {
-    const [state, setState] = React.useState({
+    const [state, setState] = useState({
         email: '',
         password: ''
     });
+
+    const [redirect, setRedirect] = useState(false)
+
     const updateState = (e) => {
         console.log('VALUE', e.target.value);
         console.log('NAME', e.target.name);
         setState({ ...state, [e.target.name]: e.target.value });
     };
+
+    // //Redirect handler
+    const handleRedirect = () => {
+        setRedirect(true);
+    };
+
+    //Redirect function
+    const renderRedirect = (user) => {
+
+        let path = "";
+        user.username ? path = "/searchprofiles" : path = "/setupprofile";
+
+
+
+        if (redirect) {
+            return (
+                <Redirect
+                    to={{
+                        pathname: path,
+                        state: {
+
+                        }
+                    }}
+                />
+            );
+        }
+    };
+
 
     const submitForm = async (e) => {
         e.preventDefault();
@@ -58,7 +92,7 @@ export default function LoginContainer() {
                 />
                 <div className="text-center">
                     <Link to="/searchprofiles">
-                        <button type="submit" className="LoginButton btn btn-light mb-4">Login</button>
+                        <button className="LoginButton btn btn-light mb-4">Login</button>
                     </Link>
                 </div>
             </form>
