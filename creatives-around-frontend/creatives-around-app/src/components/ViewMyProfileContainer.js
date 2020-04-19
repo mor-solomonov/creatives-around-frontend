156f423a5681741e3c252510432d9ac809e1b250
Nic Solitom <nicsolitom@gmail.com>
2020-04-19 03:45:56 +0200

    Remade buttons for art form

--- creatives-around-frontend/creatives-around-app/src/components/ViewMyProfileContainer.js
+++ creatives-around-frontend/creatives-around-app/src/components/ViewMyProfileContainer.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
 import '../styles/App.css';
 import TitleWithSymbol from './SymbolsandTitles/TitleWithSymbol';
 // Images:
@@ -10,104 +10,94 @@ import RemoveWhiteCircle from '../images/graphs/remove_white_circle.svg';
 import AddPicGallery from '../images/graphs/add_pic_gallery.svg';
 
 const ViewMyProfileContainer = () => {
-    return (
-        <Fragment>
-            <div className='App-Container'>
-                <TitleWithSymbol title='My Profile' />
-                
-                {/* Profile and Cover pics: */}
-                <div className='ProfilePicsContainer'>
+  return (
+    <Fragment>
+      <div className="App-Container">
+        <TitleWithSymbol title="My Profile" />
 
-                    <div className='CoverPic'>
-                        <img src={CoverPic} />
-                    </div>
+        {/* Profile and Cover pics: */}
+        <div className="ProfilePicsContainer">
+          <div className="CoverPic">
+            <img src={CoverPic} />
+          </div>
 
-                    <div className='ProfilePic'>
-                        <img src={ProfilePic} />
-                    </div>
-                    
-                </div>
+          <div className="ProfilePic">
+            <img src={ProfilePic} />
+          </div>
+        </div>
 
-                {/* Full name, username, location, age */}
-                <div className='FullNameUsername'>
-                    <h4 className='FullName'>Steve Bloom</h4>
-                    <h5>=</h5>
-                    <h4 className='Username'>Tweaker</h4>
-                </div>
+        {/* Full name, username, location, age */}
+        <div className="FullNameUsername">
+          <h4 className="FullName">Steve Bloom</h4>
+          <h5>=</h5>
+          <h4 className="Username">Tweaker</h4>
+        </div>
 
-                <div className='LocationAgeContainer'>
-                    <div className='ProfileInfoLeft'>
-                        <h5>Location</h5>
-                        <h4>Berlin</h4>
-                    </div>
-                    <div className='ProfileInfoRight'>
-                        <h5>Age</h5>
-                        <h4>33</h4>
-                    </div>
-                </div>
+        <div className="LocationAgeContainer">
+          <div className="ProfileInfoLeft">
+            <h5>Location</h5>
+            <h4>Berlin</h4>
+          </div>
+          <div className="ProfileInfoRight">
+            <h5>Age</h5>
+            <h4>33</h4>
+          </div>
+        </div>
 
-                {/* Art forms */}
-                <h4 className='FormHeader'>Art forms</h4>
+        {/* Art forms */}
 
-{/* Example of Art forms (without remove-button): */}
-<div className='ArtFormsCollection'>
+        {/* Example of Art forms (without remove-button): */}
+        <div className="ArtFormsCollection Container col-md-4 text-center">
+          <h4 className="FormHeader">Art forms</h4>
+          <button type="button" className="ArtFormSingle btn btn-outline-light">
+            <p className="ArtFormsText">Water Color</p>
+            <img src={RemoveWhiteCircle} alt="" srcset="" />
+          </button>
 
-    <div className='ArtFormSingle'>
-        <p className='ArtFormsText'>
-            Water Color
-        </p>
-    </div>
+          <button type="button" className="ArtFormSingle btn btn-outline-light">
+            <p className="ArtFormsText">Photography</p>
+            <img src={RemoveWhiteCircle} alt="" srcset="" />
+          </button>
 
-    <div className='ArtFormSingle'>
-        <p className='ArtFormsText'>
-            Photography
-        </p>
-    </div>
+          <button type="button" className="ArtFormSingle btn btn-outline-light">
+            <p className="ArtFormsText">Acrylic</p>
+            <img src={RemoveWhiteCircle} alt="" srcset="" />
+          </button>
 
-    <div className='ArtFormSingle'>
-        <p className='ArtFormsText'>
-            Acrylic
-        </p>
-    </div>
+          <button type="button" className="ArtFormSingle btn btn-outline-light">
+            <p className="ArtFormsText">Sculpture</p>
+            <img src={RemoveWhiteCircle} alt="" srcset="" />
+          </button>
 
-    <div className='ArtFormSingle'>
-        <p className='ArtFormsText'>
-            Sculpture
-        </p>
-    </div>
+          <button type="button" className="ArtFormSingle btn btn-outline-light">
+            <p className="ArtFormsText">Digital Art</p>
+            <img src={RemoveWhiteCircle} alt="" srcset="" />
+          </button>
+        </div>
 
-    <div className='ArtFormSingle'>
-        <p className='ArtFormsText'>
-            Digital Art
-        </p>
-    </div>
-    
-</div>
-
-                <div className="ViewProfileGallery">
-                    <div className="ViewProfileGallerySingle">
-                        <img src={AddPicGallery} alt=""/>
-                    </div>
-                    <div className="ViewProfileGallerySingle">
-                        <img src={AddPicGallery} alt=""/>
-                    </div>
-                    <div className="ViewProfileGallerySingle">
-                        <img src={AddPicGallery} alt=""/>
-                    </div>
-                    <div className="ViewProfileGallerySingle">
-                        <img src={AddPicGallery} alt=""/>
-                    </div>
-                    <div className="ViewProfileGallerySingle">
-                        <img src={AddPicGallery} alt=""/>
-                    </div>
-                    {/* <div className="ViewProfileGallerySingle">
+        <div className="ViewProfileGallery">
+          <div className="ViewProfileGallerySingle">
+            <img src={AddPicGallery} alt="" />
+          </div>
+          <div className="ViewProfileGallerySingle">
+            <img src={AddPicGallery} alt="" />
+          </div>
+          <div className="ViewProfileGallerySingle">
+            <img src={AddPicGallery} alt="" />
+          </div>
+          <div className="ViewProfileGallerySingle">
+            <img src={AddPicGallery} alt="" />
+          </div>
+          <div className="ViewProfileGallerySingle">
+            <img src={AddPicGallery} alt="" />
+          </div>
+          {/* <div className="ViewProfileGallerySingle">
                         <img src={AddPicGallery} alt=""/>
                     </div> */}
-                </div>
-            </div>
-        </Fragment>
-    )
-
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default ViewMyProfileContainer;
