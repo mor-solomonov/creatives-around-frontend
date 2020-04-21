c56201c3e867b8315bba220adefe421466daeaef
Mor_aka_Gaia <mor.solomonov@gmail.com>
2020-04-21 17:23:49 +0200

    Updates

---
+++ precache-manifest.3ba13aada5504944a5fa755c0d37f421.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58af2f461e17960ce4833691c938f37b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50646a7dee896ac5b1c3",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "307c9077a38931bda932",
+    "url": "/static/css/main.648ff57f.chunk.css"
+  },
+  {
+    "revision": "50646a7dee896ac5b1c3",
+    "url": "/static/js/2.c0cf1797.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c0cf1797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "307c9077a38931bda932",
+    "url": "/static/js/main.0d2c88c2.chunk.js"
+  },
+  {
+    "revision": "7b00dceb36a77614bc41",
+    "url": "/static/js/runtime-main.6f538ebf.js"
+  },
+  {
+    "revision": "3fdecc3636992786cc4260366579ea9f",
+    "url": "/static/media/about_us_box.3fdecc36.svg"
+  },
+  {
+    "revision": "57b4fea47b1dbac15daeed1d3d23a7b0",
+    "url": "/static/media/add_cover_pic.57b4fea4.svg"
+  },
+  {
+    "revision": "9799c57c92f9d4438fc9848f49615140",
+    "url": "/static/media/add_pic_gallery.9799c57c.svg"
+  },
+  {
+    "revision": "d147ebb4591d8ee9eb7157b8d3cfe442",
+    "url": "/static/media/add_profile_pic.d147ebb4.svg"
+  },
+  {
+    "revision": "25d2133fec0cf4ee4fc7e15a9d692a7c",
+    "url": "/static/media/add_travel_text.25d2133f.svg"
+  },
+  {
+    "revision": "31e5cc391266253482c5f72a222b44fe",
+    "url": "/static/media/ca_logo_white.31e5cc39.svg"
+  },
+  {
+    "revision": "f1a7b2cc5b218008f1ce833105c06710",
+    "url": "/static/media/ca_symbol_white.f1a7b2cc.svg"
+  },
+  {
+    "revision": "d4c817e1e7e93ff8382042cbba62dd33",
+    "url": "/static/media/cancel_text.d4c817e1.svg"
+  },
+  {
+    "revision": "3ab150909672968cf79d2baa11ee7ad4",
+    "url": "/static/media/connections.3ab15090.svg"
+  },
+  {
+    "revision": "0e1bf1f4bc4ab5a01e0095b7db183b96",
+    "url": "/static/media/contact_us_box.0e1bf1f4.svg"
+  },
+  {
+    "revision": "ae48f876a2b5cf617b6f679dbf1421e4",
+    "url": "/static/media/delete_account.ae48f876.svg"
+  },
+  {
+    "revision": "329c313365b0f9810107a727e136909c",
+    "url": "/static/media/location_settings_box.329c3133.svg"
+  },
+  {
+    "revision": "8113e2802923f93698fc759af2b2f94c",
+    "url": "/static/media/menu_bars.8113e280.svg"
+  },
+  {
+    "revision": "93d73c523853132f6a6002315a08be17",
+    "url": "/static/media/my_profile_box.93d73c52.svg"
+  },
+  {
+    "revision": "a2d92f97f8c6d8312c6c282a2daf2da8",
+    "url": "/static/media/o-SPIEGELMAN-570.a2d92f97.jpg"
+  },
+  {
+    "revision": "1360d0d1ed9f4a09e2cc98a3f876d59d",
+    "url": "/static/media/profile_10.1360d0d1.png"
+  },
+  {
+    "revision": "b96219b77821a95c7cf244c02433b21a",
+    "url": "/static/media/profile_11.b96219b7.png"
+  },
+  {
+    "revision": "6f6c847e18b7cc78d65ea6edf47095a6",
+    "url": "/static/media/profile_2.6f6c847e.png"
+  },
+  {
+    "revision": "54b89d37c33d01e877d3c0232d0bb81d",
+    "url": "/static/media/profile_3.54b89d37.png"
+  },
+  {
+    "revision": "3eb4fd9c449760763ba6a74137dff28e",
+    "url": "/static/media/profile_4.3eb4fd9c.png"
+  },
+  {
+    "revision": "a29632a640ba9d1caebbfedddf3aead4",
+    "url": "/static/media/profile_5.a29632a6.png"
+  },
+  {
+    "revision": "0da36ff0a23214fe5eb15925acd205ee",
+    "url": "/static/media/profile_6.0da36ff0.png"
+  },
+  {
+    "revision": "12b066471563cdf2bb0897d93cd17301",
+    "url": "/static/media/profile_7.12b06647.png"
+  },
+  {
+    "revision": "644abae0ed6d95539a1ff22fec6681eb",
+    "url": "/static/media/profile_8.644abae0.png"
+  },
+  {
+    "revision": "33ddae5958838fb762532b94b4e2fef5",
+    "url": "/static/media/profile_9.33ddae59.png"
+  },
+  {
+    "revision": "cd168b899021f45c184d2cf5f62fc879",
+    "url": "/static/media/profile_paintrbush_man.cd168b89.png"
+  },
+  {
+    "revision": "2de61467d167c7fd5b96a9726d4db78a",
+    "url": "/static/media/remove_white_circle.2de61467.svg"
+  },
+  {
+    "revision": "386b9a34d1fc193980552f9d21216a42",
+    "url": "/static/media/save_text.386b9a34.svg"
+  },
+  {
+    "revision": "d62c02f14b477081d008857bc3fb43bd",
+    "url": "/static/media/settings_text.d62c02f1.svg"
+  }
+]);
