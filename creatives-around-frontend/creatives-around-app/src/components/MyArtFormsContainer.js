76c4c8ea2502b491732f658eeab1609907859c54
Mor_aka_Gaia <mor.solomonov@gmail.com>
2020-04-24 20:26:19 +0200

    added responsivness, styles, buttons for search art forms and art forms

--- creatives-around-frontend/creatives-around-app/src/components/MyArtFormsContainer.js
+++ creatives-around-frontend/creatives-around-app/src/components/MyArtFormsContainer.js
@@ -57,12 +57,11 @@ const MyArtFormsContainer = (props) => {
     return (
 
         <div className="App-Container">
-            <TitleWithSymbol title="My Art Forms" />
+            <TitleWithSymbol title="" />
 
-            <Logo />
-            <DisplayTitle title="Select your art forms" />
-            <section className='section-container'>
-                <form className='form' onSubmit={(e) => onSubmit(e)}>
+            <DisplayTitle title="Select your art forms" className="myArtFormsTitle" />
+            <section className='section-container myArtFormsContainer'>
+                <form className='form myArtFormsForm' onSubmit={(e) => onSubmit(e)}>
                     <div className='checkbox-container'>
                         <input
                             className='checkbox'
@@ -182,19 +181,21 @@ const MyArtFormsContainer = (props) => {
                         <label className='label'>Ceramics</label>
 
                     </div>
-                    <div>
-                        <div className="SubmitButtonArtForms text-center">
-                            <button onClick={backClick} className=" btn btn-light mb-4">Submit</button>
-                        </div>
-                        <div className="GoBackButtonArtForms text-center">
-                            <button onClick={backClick} className=" btn btn-light mb-4">Go Back</button>
-                        </div>
+
+                </form>
+                <div className="text-center">
+                    <div className="SubmitButtonArtForms text-center">
+                        <button onClick={backClick} className=" btn btn-light mb-4">Submit</button>
                     </div>
+                    <div className="GoBackButtonArtForms text-center">
+                        <button onClick={backClick} className=" btn btn-light mb-4">Go Back</button>
+                    </div>
+
                     <div className="text-center">
                         <p className="cannot-find-artform">Cannot find an Art Form?&nbsp;&nbsp;
                         <Link to="/contactus"><b>Write us</b> </Link></p>
                     </div>
-                </form>
+                </div>
             </section>
         </div>
     )
