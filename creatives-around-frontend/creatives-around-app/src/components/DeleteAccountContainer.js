b02ba499ed48e8084d57df3989e25add1bf2df63
Nic Solitom <nicsolitom@gmail.com>
2020-05-24 23:59:23 +0200

    Styled delete account page

--- creatives-around-frontend/creatives-around-app/src/components/DeleteAccountContainer.js
+++ creatives-around-frontend/creatives-around-app/src/components/DeleteAccountContainer.js
@@ -1,38 +1,48 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
 import '../styles/App.css';
 import TitleWithSymbol from './SymbolsandTitles/TitleWithSymbol';
 import Logo from '../components/SymbolsandTitles/Logo';
 import { Link } from 'react-router-dom';
 
 const DeleteAccountContainer = () => {
-    return (
-        <Fragment>
-            <div className="App-Container ">
-                <TitleWithSymbol title="" />
-                <Logo />
+  return (
+    <Fragment>
+      <div className="App-Container ">
+        <TitleWithSymbol title="" />
+        <Logo />
 
-                <div className="form-group text-center">
-                    <form className='DeleteAccountform text-center'>
-                        <label for="FormControlTextarea" className="FormControlTextarea">Why would you like to delete your account? (optional)</label>
-                        <textarea class="form-control " rows="4"></textarea>
-                        <input id='checkbox' type='checkbox' name='deleteaccount' />
-                        <label className='delete-account-checkbox'>&nbsp;Delete my account</label>
-                        <div className="text-center MarginButtons">
-                            <Link to="/accountdeleted"> <button type="submit" className="btn btn btn-light mb-4 btn-block">Delete Account</button></Link>
-                            <Link className="text-decoration-none" to="/settings"> <button className="DeleteAccountbtn btn btn-light mb-4 btn-block">Go Back</button></Link>
-                        </div>
-
-
-
-                    </form>
-
-                </div>
+        <div className="form-group text-center">
+          <form className="DeleteAccountform text-center">
+            <label for="FormControlTextarea" className="FormControlTextarea">
+              Why would you like to delete your account?
+            </label>
+            <textarea class="form-control TextAreaPurple" rows="4"></textarea>
+            <input id="checkbox" type="checkbox" name="deleteaccount" />
+            <label className="delete-account-checkbox">
+              &nbsp;Delete my account
+            </label>
+            <div className="text-center MarginButtons">
+              <Link to="/accountdeleted">
+                {' '}
+                <button
+                  type="submit"
+                  className="DeleteAccountButton btn btn btn-light mb-4"
+                >
+                  Delete Account
+                </button>
+              </Link>
+              <Link className="text-decoration-none" to="/settings">
+                {' '}
+                <button className="DeleteAccountButton btn btn-light mb-4">
+                  Go Back
+                </button>
+              </Link>
             </div>
-
-
-        </Fragment>
-    )
-
+          </form>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default DeleteAccountContainer;
